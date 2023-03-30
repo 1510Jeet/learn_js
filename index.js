cc91ce4879b8d8e73e6377ddbab129ae5bd52205
1510Jeet <1510jeet@gmail.com>
2023-03-31 00:46:23 +0530

    Thanks Free Code Camp

---
+++ index.js
@@ -0,0 +1,14 @@
+//document.getElementById("count-el").innerText=5
+
+
+// let count=0
+// let firstbatch=7
+// let secondbatch=19
+
+// count = firstbatch + secondbatch
+// console.log(count)
+
+// one human year =  7 dog years
+// convert a human year to dog years
+let humanyears=7
+console.log("Your Dog Years Age is ",humanyears*7)
