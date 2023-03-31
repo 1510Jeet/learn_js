b9d39e8fc7b41b55bf0665f1d864827282624b3f
1510Jeet <1510jeet@gmail.com>
2023-03-31 11:05:53 +0530

    Made A Pasenger Counter Website using JS

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
-//document.getElementById("count-el").innerText=5
+let count = 0
+function increment(){
+    count++
+    document.getElementById("count-el").innerText=count
+}
 
-
-// let count=0
-// let firstbatch=7
-// let secondbatch=19
-
-// count = firstbatch + secondbatch
-// console.log(count)
-
-// one human year =  7 dog years
-// convert a human year to dog years
-let humanyears=7
-console.log("Your Dog Years Age is ",humanyears*7)
